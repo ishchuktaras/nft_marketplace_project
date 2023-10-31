0c4f248f1e57a9a6fd1c264165ef66363805063a
Taras Ishchuk <ishchuktaras@gmail.com>
2023-10-31 20:46:38 +0100

    Components structure and main imports

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { MdNotifications } from 'react-icons/md';
+import { BsSearch } from 'react-icons/bs';
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg';
+
+import Style from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
