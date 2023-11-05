49a429e4ac1d108841a15ef986efacb8a5e3edbd
Taras Ishchuk <ishchuktaras@gmail.com>
2023-11-05 20:11:00 +0100

    NavBar component edit

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -1,3 +1,5 @@
-import Button from './Button/Button';
-import Footer from './Footer/Footer';
-import Navbar from './Navbar/Navbar';
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+
+export { Button, Footer, NavBar };
