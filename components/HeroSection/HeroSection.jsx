1fd98660dd3b7318e80e3d054bb3ae40f9897018
Taras Ishchuk <ishchuktaras@gmail.com>
2023-11-17 12:40:33 +0100

    Hero section component enter

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+//INTERNAL IMPORT
+import Style from "./HeroSection.module.css";
+import { Button } from "../componentsindex";
+import images from "../../img";
 
 const HeroSection = () => {
+  
+  const router = useRouter();
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className={Style.heroSection}>
+      <div className={Style.heroSection_box}>
+        <div className={Style.heroSection_box_left}>
+          <h1>Discover, collect and sell NFTs 🖼️</h1>
+          <p>
+            Discover the most outstanding NTFs in all topics of life. Creative
+            your NTFs and sell them
+          </p>
+          <Button
+            btnName="Start your search"
+            handleClick={() => router.push("/searchPage")}
+          />
+        </div>
+        <div className={Style.heroSection_box_right}>
+          <Image
+            src={images.hero}
+            alt="Hero section"
+            width={600}
+            height={600}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import Style from "../styles/index.module.css";
 import { HeroSection } from "../components/componentsindex";
 
 const Home = () => {
+  
   return (
     <div className={Style.homePage}>
       <HeroSection />
+     
     </div>
   );
 };
