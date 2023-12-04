52355578d8fc099a8025bcc4e29bca61039139fc
Taras Ishchuk <ishchuktaras@gmail.com>
2023-12-04 20:40:07 +0100

    SliderCards Section add

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import {
   Collection,
   FollowerTab,
   AudioLive,
+  Slider,
 } from "../components/componentsindex";
 
 const Home = () => {
@@ -28,6 +29,7 @@ const Home = () => {
       />
       <AudioLive />
       <FollowerTab />
+      <Slider />
       <Collection />
       <Title
         heading="Featured NTFs"
